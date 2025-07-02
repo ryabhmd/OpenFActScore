b8f174811596076426bada7ddd01cc20447f8873
lflage <flage.lucas@gmail.com>
2025-07-02 17:14:53 +0200

    debug: generate_facts now skips if there are no atomic facts for sentence

--- generate_facts.py
+++ generate_facts.py
@@ -1,9 +1,12 @@
+""" Generate Atomic Facts and evalutes them according to BertScore strategy
+"""
 import os
 import sys
 import logging
 import json
 import argparse
 import nltk
+from tqdm import tqdm
 from factscore.atomic_facts import AtomicFactGenerator
 from metrics.bs import AFGBertScores
 from envs import ROOT
@@ -17,10 +20,25 @@ print(f"started: {__file__}")
 
 parser = argparse.ArgumentParser(description="Command-line interface for Atomic Fact Generation.")
 
-parser.add_argument("--hf_model", type=str, default="meta-llama/Llama-3.1-8B-Instruct", help="Hugging Face model path")
-parser.add_argument("--examples_path", type=str, default="/netscratch/fonseca/OpenFActScore/data/labeled/InstructGPT.jsonl", help="Path to the examples file")
-parser.add_argument("--out_dir", type=str, default="results/metrics/afg_bert_score/", help="Output directory")
-parser.add_argument("--report_path", type=str, default=None, help="Path to the report file (optional)")
+parser.add_argument("--hf_model",
+                    type=str,
+                    default="meta-llama/Llama-3.1-8B-Instruct",
+                    help="Hugging Face model path")
+
+parser.add_argument("--examples_path",
+                    type=str,
+                    default=f"{ROOT}/data/labeled/InstructGPT.jsonl",
+                    help="Path to the examples file")
+
+parser.add_argument("--out_dir",
+                    type=str,
+                    default="results/metrics/afg_bert_score/",
+                    help="Output directory")
+                  
+parser.add_argument("--report_path",
+                    type=str,
+                    default=None,
+                    help="Path to the report file (optional)")
 
 args = parser.parse_args()
 
@@ -44,7 +62,7 @@ print(f"Output Path: {output_path}")
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
-                        filename="/netscratch/fonseca/OpenFActScore/afg_llama.log",
+                        filename=os.path.join(ROOT,"model_name.log"),
                         filemode='w',
                         level=logging.DEBUG)
 
@@ -52,13 +70,13 @@ generator = AtomicFactGenerator(HF_MODEL, ".cache/factscore/demos", af_cache_fil
 
 final_jsonl = []
 with open (EXAMPLES_PATH, encoding="utf-8", mode="r") as f:
-    for line in f:
+    for line in tqdm(f):
         cur_ex = json.loads(line)
         atomic_facts, para_breaks = generator.run(cur_ex["output"])
         try:
             assert len(atomic_facts) == len(cur_ex["annotations"])
-        except TypeError:
-            logger.debug("Model: %s Atomic Facts: %s\nCurrent_annotations %s\n",
+        except (TypeError, AssertionError):
+            logger.debug("TypeError in:\nModel: %s Atomic Facts: %s\nCurrent_annotations %s\n",
                         HF_MODEL,
                         atomic_facts,
                         cur_ex["annotations"])
@@ -66,7 +84,8 @@ with open (EXAMPLES_PATH, encoding="utf-8", mode="r") as f:
         # cur_ex["annotations"].append({"llama-atomic-facts":
         # [{"text": str}]})
         for atom, annotation in zip(atomic_facts, cur_ex["annotations"]):
-            assert atom[0] == annotation["text"]
+            if atom is None:
+                continue
             annotation['model-atomic-facts'] = [{"text": fact} for fact in atom[1]]
 
         final_jsonl.append(cur_ex)
@@ -78,8 +97,9 @@ with open(output_path, encoding="utf-8", mode="w") as out_f:
         logger.debug("item:\n%s", json.dumps(item))
         out_f.write(json.dumps(item) + "\n")
 
-# TODO: Get BertScores for the generations
 
-bs = AFGBertScores(output_path)
 
-bs.get_report(report_path=REPORT_PATH)
+bs = AFGBertScores(output_path, mode="human_afs")
+bs.compute_scores()
+bs.to_csv()
+bs.get_report()
